5eb0a99b89d84344551118bc2de8f7fb56fb4191
Abdullah Ansari <92114680+abdullahtech11@users.noreply.github.com>
2022-05-17 15:52:09 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import "./css/header.css"
+
+
+
+function App() {
+  return (
+    <div className="App">
+     {/*Header */}
+     <div className="header-div">
+     {/*left-Header */}
+    <div className="header-left">
+     <div className="logo-div"><img src="https://z.nooncdn.com/s/app/com/noon/images/logos/noon-black-en.svg" alt="logo"></img></div>
+     <div className="flag-div"><img src="https://z.nooncdn.com/s/app/com/common/images/flags/ae.svg" alt="flag"></img></div>
+     <div className="text-div"><p>Deliver to <br/><strong>Dubai</strong></p></div>
+     </div>
+     {/*mid-Header */}
+     <div className="seacrch-bar"><input type="text" id="searchBar" placeholder="What are you looking for?"></input></div>
+     
+     {/*right-Header */}
+     </div>
+     
+     {/*App body */}
+     {/*left side bar */}
+     {/*post side bar*/}
+     {/*right side bar*/}
+    </div>
+  );
+}
+
+export default App;
